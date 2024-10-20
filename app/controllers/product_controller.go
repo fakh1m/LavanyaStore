2d1db09e074e8ce311ed82e093b2ebe7358185c6
Imam Fahrudin <fakhim.ilm@gmail.com>
2024-10-20 19:39:06 +0700

    add product detail

--- app/controllers/product_controller.go
+++ app/controllers/product_controller.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"github.com/fakh1m/LavanyaStore/app/models"
+	"github.com/gorilla/mux"
 	"github.com/unrolled/render"
 )
 
@@ -46,4 +47,25 @@ func (server *Server) Products(w http.ResponseWriter, r *http.Request) {
 		"products":   products,
 		"pagination": pagination,
 	})
+}
+
+func (server *Server) GetProductBySlug(w http.ResponseWriter, r *http.Request) {
+	render := render.New(render.Options{
+		Layout: "layout",
+	})
+	vars := mux.Vars(r)
+
+	if vars["slug"] == "" {
+		return
+	}
+
+	productModel := models.Product{}
+	product, err := productModel.FindBySlug(server.DB, vars["slug"])
+	if err != nil {
+		return
+	}
+	_ = render.HTML(w, http.StatusOK, "product", map[string]interface{}{
+		"product": product,
+	})
+
 }
--- app/controllers/routes.go
+++ app/controllers/routes.go
@@ -10,8 +10,10 @@ func (server *Server) initializeRoutes() {
 	server.Router = mux.NewRouter()
 	server.Router.HandleFunc("/", server.Home).Methods("GET")
 	server.Router.HandleFunc("/products", server.Products).Methods("GET")
+	server.Router.HandleFunc("/products/{slug}", server.GetProductBySlug).Methods("GET")
 
 	staticFileDirectory := http.Dir("./assets/")
 	staticFileHandler := http.StripPrefix("/public/", http.FileServer(staticFileDirectory))
 	server.Router.PathPrefix("/public/").Handler(staticFileHandler).Methods("GET")
+
 }
